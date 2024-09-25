d7aa320b87e216396eae4f21196afc0a7505207a
Samuel <samuellamilla@hotmail.com>
2024-09-25 17:44:20 -0500

    SPRING

---
+++ zoofantastico/src/main/java/com/lab/zoofantastico/ZoofantasticoApplication.java
@@ -0,0 +1,13 @@
+package com.lab.zoofantastico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoofantasticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoofantasticoApplication.class, args);
+	}
+
+}
---
+++ zoofantastico/src/test/java/com/lab/zoofantastico/ZoofantasticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.zoofantastico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoofantasticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
